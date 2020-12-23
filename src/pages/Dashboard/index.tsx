f9d53b74a298e59e549fcf22c23159407d8b23bb
Matheus Afonso <matheusafonsouza@gmail.com>
2020-12-23 14:12:09 -0300

    fix: loading pokemons from api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -23,155 +23,111 @@ interface CategoryResponse {
   results: Category[];
 }
 
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokemonResponse {
+  count: number;
+  results: Category[];
+}
+
 const Dashboard: React.FC = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    async function loadCategories() {
-      const response = await api.get<CategoryResponse>('type');
+    async function loadInformation() {
+      const categoriesResponse = await api.get<CategoryResponse>('type');
+      const pokemonsResponse = await api.get<PokemonResponse>('pokemon', {
+        params: {
+          offset: 0,
+          limit: 1118,
+        },
+      });
 
-      setCategories(response.data.results);
+      setCategories(categoriesResponse.data.results);
+      setPokemons(pokemonsResponse.data.results);
     }
 
-    loadCategories();
+    loadInformation();
   }, []);
 
   return (
     <Container>
       <Sidebar>
         {categories.map((category: Category) => (
-          <Category>{category.name}</Category>
+          <Category>{category.name.toUpperCase()}</Category>
         ))}
       </Sidebar>
       <Content>
-        <PokemonItem>
-          <Pokemon>
-            <img
-              src="https://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/1200px-006Charizard.png"
-              alt="pokemon"
-            />
-            <PokemonDescription>
-              <span>Charizard</span>
-              <p>Pokemon maneiro</p>
-            </PokemonDescription>
-            <PokemonTypes>
-              <span>Dragão</span>
-              <span>Fogo</span>
-            </PokemonTypes>
-          </Pokemon>
-          <PokemonData>
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
+        {pokemons.map((pokemon: Pokemon) => (
+          <PokemonItem>
+            <Pokemon>
+              <img
+                src="https://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/1200px-006Charizard.png"
+                alt="pokemon"
+              />
+              <PokemonDescription>
+                <span>{pokemon.name}</span>
+                <p>Pokemon maneiro</p>
+              </PokemonDescription>
+              <PokemonTypes>
+                <span>Dragão</span>
+                <span>Fogo</span>
+              </PokemonTypes>
+            </Pokemon>
+            <PokemonData>
+              <PokemonDetail>
+                <span>Speed</span>
+                <p>130</p>
+                <div>
+                  <div />
+                </div>
+              </PokemonDetail>
+              <PokemonDetail>
+                <span>Speed</span>
+                <p>130</p>
+                <div>
+                  <div />
+                </div>
+              </PokemonDetail>
 
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
+              <PokemonDetail>
+                <span>Speed</span>
+                <p>130</p>
+                <div>
+                  <div />
+                </div>
+              </PokemonDetail>
 
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
+              <PokemonDetail>
+                <span>Speed</span>
+                <p>130</p>
+                <div>
+                  <div />
+                </div>
+              </PokemonDetail>
 
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
+              <PokemonDetail>
+                <span>Speed</span>
+                <p>130</p>
+                <div>
+                  <div />
+                </div>
+              </PokemonDetail>
 
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-          </PokemonData>
-        </PokemonItem>
-        <PokemonItem>
-          <Pokemon>
-            <img
-              src="https://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/1200px-006Charizard.png"
-              alt="pokemon"
-            />
-            <PokemonDescription>
-              <span>Charizard</span>
-              <p>Pokemon maneiro</p>
-            </PokemonDescription>
-            <PokemonTypes>
-              <span>Dragão</span>
-              <span>Fogo</span>
-            </PokemonTypes>
-          </Pokemon>
-          <PokemonData>
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-
-            <PokemonDetail>
-              <span>Speed</span>
-              <p>130</p>
-              <div>
-                <div />
-              </div>
-            </PokemonDetail>
-          </PokemonData>
-        </PokemonItem>
+              <PokemonDetail>
+                <span>Speed</span>
+                <p>130</p>
+                <div>
+                  <div />
+                </div>
+              </PokemonDetail>
+            </PokemonData>
+          </PokemonItem>
+        ))}
       </Content>
     </Container>
   );
